5ac69cff384e6ceb982cd8e21b12a0e0db09c5ee
ccgogo123 <xhhuangxi@gmail.com>
2015-08-23 11:55:34 -0500

    Created Snake class and completed the draw method

--- public/scripts/snake.js
+++ public/scripts/snake.js
@@ -1,3 +1,21 @@
 /**
  * Created by christopherhuang on 8/23/15.
  */
+function Snake() {
+    this.direction = 'Right';
+    this.height = 10;
+    this.width = 10;
+    this.color = 'red';
+    this.body = [[0, 0]];
+}
+
+Snake.prototype.draw = function(canvas) {
+    var context = canvas.getContext('2d');
+    context.fillStyle = this.color;
+    this.body.forEach(function(cell) {
+        context.fillRect(cell[0], cell[1], this.height, this.width);
+    }, this);
+}
+
+var canvas = document.getElementById('canvas');
+new Snake().draw(canvas);
